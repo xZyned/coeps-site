a3089acab8f8a558711bf1344dd5d0aa169f5295
TeteuTias <46979510+TeteuTias@users.noreply.github.com>
2026-06-10 08:48:21 -0300

    Pagamento Crédito Ok

--- app/api/v1/payment/session/creditCard/route.ts
+++ app/api/v1/payment/session/creditCard/route.ts
@@ -2,6 +2,24 @@ import { ObjectId } from 'mongodb';
 import { withApiAuthRequired } from '@/lib/auth0-compat';
 import { connectToDatabase } from '@/app/lib/mongodb';
 import { getUserId } from '@/lib/getUserId';
+import PaymentTicketProps from '@/lib/types/payments/paymentTicket.t';
+//
+//
+//
+function formatarData(data: Date): string {
+    data.setDate(data.getDate());
+
+    // 3. Montamos no formato exato que o Asaas exige: YYYY-MM-DD
+    const ano = data.getFullYear();
+    // O getMonth() começa no zero (Janeiro = 0), por isso o +1.
+    // O padStart(2, '0') garante que o mês 5 vire "05"
+    const mes = String(data.getMonth() + 1).padStart(2, '0');
+    const dia = String(data.getDate()).padStart(2, '0');
+
+    const dueDateFormatado = `${ano}-${mes}-${dia}`;
+    return dueDateFormatado
+}
+//
 //
 //
 /**
@@ -9,8 +27,15 @@ import { getUserId } from '@/lib/getUserId';
  * Antes de criar o pagamento, ele verifica se a sessão do usuário ainda é válida.
  */
 export const POST = withApiAuthRequired(async function POST(request) {
+    // Captando ip para pagamento
+    const forwardedFor = request.headers.get('x-forwarded-for');
+    // Se houver mais de um IP na lista, pega o primeiro
+    const ip = forwardedFor ? forwardedFor.split(',')[0].trim() : request.socket.remoteAddress;
+
+    //
     try {
         //
+        const now = new Date();
         const dataPayment = await request.json()
         const userId = await getUserId(request)
         //
@@ -29,10 +54,104 @@ export const POST = withApiAuthRequired(async function POST(request) {
         const user = await db.collection("usuarios").findOne({ _id: new ObjectId(userId) }, { projection: { "_id": 0, "id_api": 1 } })
         if (!user) {
             return Response.json({ "message": "Usuário não encontrado.", data: dataPayment }, { status: 404 })
-
         }
-        
-        return Response.json({ "message": "Em desenvolvimento.", data: dataPayment }, { status: 200 })
+        //
+        const sessaoAtiva: PaymentTicketProps | null = await db.collection('pagamentos.sessoes').findOne({
+            owner: new ObjectId(userId),
+            type: "ticket",
+            expiresAt: { $gt: now } // O segredo está aqui: expiresAt maior que o momento atual
+        });
+        // Se a sessão não existe:
+        if (!sessaoAtiva) {
+            return Response.json({ message: "Configuração não encontrada" }, { status: 404 });
+        }
+        //
+        //
+        const ASAAS_API_URL = process.env.ASAAS_API_URL
+        const ASAAS_API_KEY = process.env.ASAAS_API_KEY
+        const ASAAS_URL_CALLBACK = process.env.ASAAS_URL_CALLBACK
+        const ASAAS_URL_REDIRECT = process.env.ASAAS_URL_REDIRECT
+        //
+        //
+        //
+        // Buscando o Id de pagamento
+        // O Id do pagamento vuscado deve ser idêntico ao id pré-existente na sessão ativa
+        // Automaticamente no próprio sistema, há apenas um código por pagamento.
+        // Essa regra apenas não será verdadeira se o pagamento for criado diretamente no banco de dados
+        // e o autor não trocar o código.
+
+
+        const pagamentoSelecionado = sessaoAtiva.paymentConfig.precos.parcelamentos.find(
+            (p) => p.codigo === dataPayment.idPagamento // <-- Sem chaves, retorna automaticamente
+        );
+        if (!pagamentoSelecionado) {
+            return Response.json({ erro: "Pagamento não encontrado." }, { status: 404 });
+        }
+
+        console.log(dataPayment)
+
+        //
+        const options = {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                access_token: ASAAS_API_KEY
+            },
+            body: JSON.stringify({
+                "customer": user.id_api,
+                "billingType": "CREDIT_CARD",
+                "installmentCount": pagamentoSelecionado.totalParcelas,
+                "installmentValue": pagamentoSelecionado.valorCadaParcela,
+                "dueDate": formatarData(now),
+                "creditCard": {
+                    "holderName": dataPayment.personalInfo.name,
+                    "number": dataPayment.cardInfo.number,
+                    "expiryMonth": dataPayment.cardInfo.expiry.split("/")[0],
+                    "expiryYear": dataPayment.cardInfo.expiry.split("/")[1],
+                    "ccv": dataPayment.cardInfo.cvc
+                },
+                "creditCardHolderInfo": {
+                    "name": dataPayment.personalInfo.name,
+                    "email": dataPayment.personalInfo.email,
+                    "cpfCnpj": dataPayment.personalInfo.cpfCnpj,
+                    "postalCode": dataPayment.personalInfo.postalCode,
+                    "addressNumber": dataPayment.personalInfo.addressNumber,
+                    "addressComplement": "",
+                    "phone": dataPayment.personalInfo.phone
+                },
+                "remoteIp": ip
+            })
+        };
+        const responseAPI = await fetch(ASAAS_API_URL, options)
+        if (!responseAPI.ok) {
+            var responseJson = await responseAPI.json()
+            return Response.json({ "message": responseJson['errors'][0]['description'], "code": responseJson['errors'][0]['code'] }, { status: 500 })
+        }
+        var responseJson = await responseAPI.json()
+        // Registrando o pagamento no DB;
+        // Registrando o pagamento no banco de dados;
+        const dbUpdateOne = await db.collection("usuarios").updateOne(
+            {
+                _id: new ObjectId(userId)
+            },
+            {
+                "$push": { 'pagamento.lista_pagamentos': { ...responseJson, "description": dataPayment.personalInfo.name, _webhook: [], _type: "ticket", _userId: userId } },
+                "$set": { 'pagamento.situacao': 2 }
+            }
+        )
+        if (dbUpdateOne.matchedCount === 0) { //Nenhum documento correspondeu ao filtro.
+            console.log("result.matchedCount === 0")
+            return Response.json({ "erro": "result.matchedCount - dbUpdateOne" }, { status: 404 })
+        }
+        else if (dbUpdateOne.modifiedCount === 0) { // Nenhum documento foi modificado.
+
+            return Response.json({ "erro": "result.modifiedCount === 0 - dbUpdateOne" }, { status: 400 })
+        }
+        //
+        //
+        //
+        return Response.json({ "message": "Seu pagamento foi realizado com sucesso! Dentro de 03 dias úteis ele será confirmado e você tera acesso total ao site! Clique em recarregar para prosseguir." }, { status: 200 })
     }
     catch (error) {
 
