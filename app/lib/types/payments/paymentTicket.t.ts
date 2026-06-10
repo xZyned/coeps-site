8ccbae740e1c4a8db5dc2582e2e0a56dd73bfe7f
TeteuTias <46979510+TeteuTias@users.noreply.github.com>
2026-06-10 09:04:39 -0300

    Adicionando status ao ticket de pagamento

--- app/lib/types/payments/paymentTicket.t.ts
+++ app/lib/types/payments/paymentTicket.t.ts
@@ -20,6 +20,7 @@ export default interface PaymentTicketProps {
     },
     paymentConfig: ILoteAutomatico;
     type: "ticket" | "course";
+    status: "PENDING" | "PAID" | "UNPAID";
     paymentUrl: string;
     expiresAt: Date;
 }
